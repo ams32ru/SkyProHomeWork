8139ae9afa136ef0d38b536ddc9d4d5d6263abbc
ams32ru <ams32ru@mail.ru>
2022-10-03 12:59:34 +0300

    Забыл сделать первый коммит, передалал

--- src/homeWorkObjectClass/Author.java
+++ src/homeWorkObjectClass/Author.java
@@ -4,8 +4,8 @@ public class Author {
     private String firstName;
     private String lastName;
     public Author(String firstName, String lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
     public String getFirstName() {
         return this.firstName;
